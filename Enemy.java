f0ccf91a54b67d65fac6314f71747147d90938c2
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-07-05 12:50:07 +0530

    first commit

--- Enemy.java
+++ Enemy.java
@@ -1,16 +1,31 @@
 package AdventureGame;
 
 import java.io.Serializable;
+import java.util.Random;
 
 public class Enemy implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     String name;
     int hp;
-    int attack;
+    int baseAttack;
+    int defense;
+    String type;
+    int lootGold;
+    int xpReward;
+    String specialAbility;
 
-    public Enemy(String name, int hp, int attack) {
+    transient Random rand = new Random(); // not serialized
+
+    public Enemy(String name, int hp, int baseAttack, int defense, String type, int lootGold, int xpReward, String specialAbility) {
         this.name = name;
         this.hp = hp;
-        this.attack = attack;
+        this.baseAttack = baseAttack;
+        this.defense = defense;
+        this.type = type;
+        this.lootGold = lootGold;
+        this.xpReward = xpReward;
+        this.specialAbility = specialAbility;
     }
 
     public boolean isAlive() {
@@ -18,15 +33,32 @@ public class Enemy implements Serializable {
     }
 
     public void takeDamage(int damage) {
-        hp -= damage;
+        int actualDamage = Math.max(0, damage - defense);
+        hp -= actualDamage;
+        System.out.println(name + " takes " + actualDamage + " damage (after defense " + defense + ").");
     }
 
     public int attack() {
-        return attack;
+        int variability = rand.nextInt(5); // +/- variability
+        int totalAttack = baseAttack + variability;
+        System.out.println(name + " attacks for " + totalAttack + " damage.");
+        return totalAttack;
+    }
+
+    public int getLootGold() {
+        return lootGold;
+    }
+
+    public int getXpReward() {
+        return xpReward;
+    }
+
+    public String getSpecialAbility() {
+        return specialAbility;
     }
 
     @Override
     public String toString() {
-        return name + " (HP: " + hp + ")";
+        return name + " [" + type + "] (HP: " + hp + ", ATK: " + baseAttack + ", DEF: " + defense + ")";
     }
 }
